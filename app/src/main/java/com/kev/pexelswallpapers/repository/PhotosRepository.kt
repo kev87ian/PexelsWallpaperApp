e711e814e7190480caa378660d53d0194ff27f03
kev87ian <kelvinian87@gmail.com>
2023-04-21 21:28:05 +0300

    create a sealed class to managee error handling, wrote repository function for fetching photo details

--- app/src/main/java/com/kev/pexelswallpapers/repository/PhotosRepository.kt
+++ app/src/main/java/com/kev/pexelswallpapers/repository/PhotosRepository.kt
@@ -7,9 +7,14 @@ import androidx.paging.PagingData
 import com.kev.pexelswallpapers.data.local.PhotosDatabase
 import com.kev.pexelswallpapers.data.paging.PhotosRemoteMediator
 import com.kev.pexelswallpapers.data.remote.PhotosApiService
+import com.kev.pexelswallpapers.model.photo_details.PhotoDetailsResponse
 import com.kev.pexelswallpapers.model.photos_list.Photo
+import com.kev.pexelswallpapers.util.Resource
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import okio.IOException
+import retrofit2.HttpException
 
 class PhotosRepository @Inject constructor(
     private val apiService: PhotosApiService,
@@ -28,5 +33,24 @@ class PhotosRepository @Inject constructor(
             pagingSourceFactory = pagingSource
 
         ).flow
+    }
+
+    suspend fun getPhotoDetails(photoId: Int): Flow<Resource<PhotoDetailsResponse>> = flow {
+        emit(Resource.Loading())
+        try {
+            val response = apiService.getImageDetails(photoId)
+            emit(Resource.Success(response))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            when (e) {
+                is IOException -> emit(
+                    Resource.Error("Ensure you have an active internet connection")
+                )
+                is HttpException -> emit(
+                    Resource.Error("We're unable to reach the servers. Please retry")
+                )
+                else -> emit(Resource.Error(e.localizedMessage ?: "An error occurred"))
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/kev/pexelswallpapers/util/Resource.kt
@@ -0,0 +1,8 @@
+package com.kev.pexelswallpapers.util
+
+sealed class Resource<T> {
+
+    class Loading<T>: Resource<T>()
+    data class Error<T>(val errorMessage: String) : Resource<T>()
+    data class Success<T>(val data: T) : Resource<T>()
+}
