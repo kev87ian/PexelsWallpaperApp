d00c24a0c3e7294a2c22da06e4f07760baf424ff
kev87ian <kelvinian87@gmail.com>
2023-04-22 13:43:14 +0300

    wrote repository function for the searching photos feature

--- app/src/main/java/com/kev/pexelswallpapers/repository/PhotosRepository.kt
+++ app/src/main/java/com/kev/pexelswallpapers/repository/PhotosRepository.kt
@@ -2,6 +2,7 @@ package com.kev.pexelswallpapers.repository
 
 import com.kev.pexelswallpapers.data.remote.PhotosApiService
 import com.kev.pexelswallpapers.model.photo_details.PhotoDetailsResponse
+import com.kev.pexelswallpapers.model.photo_search.PhotoSearchResponse
 import com.kev.pexelswallpapers.util.Resource
 import javax.inject.Inject
 import okio.IOException
@@ -26,7 +27,24 @@ class PhotosRepository @Inject constructor(
                     "We're unable to reach servers. Please retry."
                 )
 
-                else -> return Resource.Error(e.localizedMessage ?: "An unknown error occurred.")
+                else -> return Resource.Error(e.localizedMessage ?: "An unknown error occurred. Please retry.")
+            }
+        }
+    }
+
+
+    suspend fun searchPhoto(photoQuery:String): Resource<PhotoSearchResponse>{
+        return try {
+
+            val response = apiService.searchPhotos(photoQuery)
+            return Resource.Success(response)
+
+        }catch (e:Exception){
+            e.printStackTrace()
+            when(e){
+                is IOException -> return Resource.Error("Ensure you have an active internet connection")
+                is HttpException -> return Resource.Error("We're unable to reach servers. Please retry.")
+                else -> return Resource.Error(e.localizedMessage ?: "An unknown error occurred. Please retry.")
             }
         }
     }
